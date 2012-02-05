02a58082948fa950574cba31d3e888e8bf3ea7ce
Tomomasa Terazaki <tomomasatomo@gmail.com>
2012-02-04 23:12:33 -0800

    Heroku 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
 
   def show
+    session[:back] = true
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
@@ -8,18 +9,33 @@ class MoviesController < ApplicationController
 
   def index
     if params[:sort]
-      @sort = params[:sort]
+      @sort= params[:sort]
+      session[:sort] = @sort
+    elsif session[:sort]
+      @sort = session[:sort]
     end
-    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    @all_ratings = Movie.get_all_ratings
     @where_statement = ""
     @not_first = params[:not_first]
+    if session[:not_first]
+      @not_first = session[:not_first]
+    end
     if params[:ratings]
       @checked_ratings = params[:ratings]
+      session[:checked_ratings] = @checked_ratings
     elsif !@not_first
       @not_first = true
+      session[:not_first] = true
       @checked_ratings = {'G'=>1, 'PG'=>1, 'PG-13'=>1, 'R'=>1}
+      session[:checked_ratings] = @checked_ratings
+    elsif session[:back] == true && session[:checked_ratings]
+      @checked_ratings = session[:checked_ratings]
+    else
+      @checked_ratings = nil
+      session[:checked_ratings] = nil
     end
     where_maker
+    session[:back] = false
     @movies = Movie.where(@where_statement).order(@sort).all
     end
 
@@ -39,16 +55,19 @@ class MoviesController < ApplicationController
   end
 
   def create
+    session[:back] = true
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
 
   def edit
+    session[:back] = true
     @movie = Movie.find params[:id]
   end
 
   def update
+    session[:back] = true
     @movie = Movie.find params[:id]
     @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
@@ -56,6 +75,7 @@ class MoviesController < ApplicationController
   end
 
   def destroy
+    session[:back] = true
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+
+  @@all_ratings = ['G', 'PG', 'PG-13', 'R']
+
+  def self.get_all_ratings
+    @@all_ratings
+  end
+
 end
